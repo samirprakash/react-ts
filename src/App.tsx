d26a65b087902e8cd4c9d48a40cf859552f93fd8
Samir Prakash <Samir.Prakash@gmail.com>
2022-04-02 18:36:52 -0500

    handle object as props in TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css';
 import Greet from './components/greet/greet.component';
+import Person from './components/person/person.component';
 
 function App() {
+  const personName = {
+    first: 'Bruce',
+    last: 'Wayne',
+  };
+
   return (
     <div className="App">
       <Greet name="Samir" msgcount={20} isLoggedin={true} />
+      <Person name={personName} />
     </div>
   );
 }
