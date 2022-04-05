bbfe70900d20bcaedde5649c0b583c1b69f30c02
Samir Prakash <Samir.Prakash@gmail.com>
2022-04-04 21:35:16 -0500

    handle styles as props in TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import BasicProps from './pages/basic-props/basic-props.component';
+import { default as ContainerProps } from './pages/container-props/container-props.component';
 import EventProps from './pages/event-props/event-props.component';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <BasicProps />
       <EventProps />
+      <ContainerProps />
     </div>
   );
 }
---
+++ src/pages/container-props/container-props.component.tsx
@@ -0,0 +1,7 @@
+import Container from '../../components/container/container.component';
+
+const ContainerProps = () => {
+  return <Container styles={{ border: '1px solid red', padding: '1rem' }} />;
+};
+
+export default ContainerProps;
